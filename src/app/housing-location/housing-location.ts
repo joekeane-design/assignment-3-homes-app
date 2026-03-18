b9f38ca4bec54119a84b2d236c9a6e10477ef4d3
joe keane <jkeane1005@gmail.com>
2026-03-18 18:14:16 -0500

    integrated details page into the application

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Housinglocationinfo } from '../housinglocationinfo';
+import { RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
 })
