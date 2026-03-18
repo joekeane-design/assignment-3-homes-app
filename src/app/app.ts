4ff2831022e59d0bc7234ab044cdab4dad85cf39
joe keane <jkeane1005@gmail.com>
2026-03-18 18:05:37 -0500

    added routing to the angular application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home'
+import {RouterModule, RouterLink} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterModule, Home, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
